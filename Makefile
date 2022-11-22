4fda40b316db66eec48dcc27c9dd3625f01edc90
Matias Christensen <mattivc@gmail.com>
2022-11-22 21:48:30 +0100

    Video 2

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CXX := g++
 
+CFLAGS := -std=c++17
 INCLUDE := -Iglad/include
 LIBS := -lglfw
 
@@ -7,7 +8,11 @@ vofs: vofs.o glad.o
 	$(CXX) vofs.o glad.o $(LIBS) -o vofs
 
 vofs.o: main.cc
-	$(CXX) -c main.cc $(INCLUDE) -o vofs.o
+	$(CXX) -c main.cc $(CFLAGS) $(INCLUDE) -o vofs.o
 
 glad.o:
-	$(CXX) -c glad/src/glad.c $(INCLUDE) -o glad.o
+	$(CXX) -c glad/src/glad.c $(CFLAGS) $(INCLUDE) -o glad.o
+
+.PHONY: clean
+clean:
+	rm -f glad.o vofs.o vofs
--- main.cc
+++ main.cc
@@ -1,12 +1,17 @@
 #include <cstdio>
+#include <iostream>
+#include <vector>
+
+#include <string_view>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 // TODO(Matias):
 // Add libraries:
-//  - glfw
-//  - glad
 //  - stb_image
 
 void glfw_error_callback(int error, const char* description) {
@@ -25,13 +30,66 @@ void glfw_key_callback(GLFWwindow* window_ptr,
 
 }
 
+
+std::vector<std::string> get_image_paths(const std::string& dataset_path) {
+  const std::string rgb_file_path = dataset_path + "rgb.txt";
+
+  FILE* fp = fopen(rgb_file_path.data(), "r");
+
+  if (fp == nullptr) {
+    fprintf(stderr, "ERROR! Unable to load rgb file: %s\n", rgb_file_path.data());
+  }
+  
+  char* line_ptr = nullptr;
+  size_t n = 0;
+  ssize_t read = 0;
+
+  getline(&line_ptr, &n, fp);
+  getline(&line_ptr, &n, fp);
+  getline(&line_ptr, &n, fp);
+
+  std::vector<std::string> image_paths;
+
+  while ((read = getline(&line_ptr, &n, fp)) != -1) {
+    std::string_view line_sw(line_ptr);
+    const auto space_position = line_sw.find(' ');
+
+    /* std::string_view timestamp_sw = line_sw.substr(0, space_position); */
+    /* std::string timestamp_str = std::string(timestamp_sw); */
+    
+    std::string_view path_sw = line_sw.substr(space_position + 1, line_sw.size() - space_position - 2);
+    
+    image_paths.emplace_back(path_sw);
+  }
+
+  return image_paths;
+}
+
+
+struct Image {
+  int width;
+  int height;
+  int channels;
+  unsigned char* data_ptr;
+};
+
 int main() {
+  std::string dataset_path("dataset/rgbd_dataset_freiburg3_long_office_household/");
+
+  std::vector<std::string> image_paths =  get_image_paths(dataset_path);
+  
+  std::string image_path = dataset_path + image_paths.at(0);
+  
+  Image image = {};
+  image.data_ptr = stbi_load(image_path.data(), &image.width, &image.height, &image.channels, 0);
+  
+  std::cout << "Image: " << image.width << "x" << image.height << ":" << image.channels << '\n';
+
+  glfwSetErrorCallback(glfw_error_callback);
 
   if (!glfwInit()) {
     fprintf(stderr, "ERROR! Unable to init GLFW\n");
   }
-
-  glfwSetErrorCallback(glfw_error_callback);
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -48,6 +106,42 @@ int main() {
   glfwSetKeyCallback(window_ptr, glfw_key_callback);
 
   glfwSwapInterval(1);
+
+  float verticies[] = {
+    // x   y    z     u   v
+    0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+    -0.5f, 0.5f, 0.0f, 0.0f, 1.0f
+  };
+
+  unsigned int indicies[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3 // second triangle
+  };
+
+  unsigned int vbo;
+  unsigned int vao;
+  unsigned int ebo;
+
+  glGenBuffers(1, &vbo);
+  glGenBuffers(1, &ebo);
+  glGenVertexArrays(1, &vao);
+  
+  glBindVertexArray(vao);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+  
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(0));
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+  glEnableVertexAttribArray(0);
+
 
   while (!glfwWindowShouldClose(window_ptr)) {
 
